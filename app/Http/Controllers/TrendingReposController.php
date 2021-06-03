2759d615ed8aecdb3615fd31abfdf223dd8262a3
Mahmoud-Morgan <mhmoudmorgan@outlook.com>
2021-06-03 09:52:01 +0200

    adding route and controller, and passing TrendingReposTest

---
+++ app/Http/Controllers/TrendingReposController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TrendingReposController extends Controller
+{
+    public function trendingLanguages()
+    {
+        return null;
+    }
+}
--- tests/Feature/TrendingReposTest.php
+++ tests/Feature/TrendingReposTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class TrendingRepos extends TestCase
+class TrendingReposTest extends TestCase
 {
     /**
      * A basic feature test example.
