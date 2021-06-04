f9065af0fde61901d3c2786e545390818167009d
Mahmoud-Morgan <mhmoudmorgan@outlook.com>
2021-06-04 16:13:09 +0200

    adding setResponse function

--- app/Http/Controllers/TrendingReposController.php
+++ app/Http/Controllers/TrendingReposController.php
@@ -2,12 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class TrendingReposController extends Controller
 {
+    private $response;
+
+    public function __construct()
+    {
+        $this->setResponse();
+    }
+
+    private function setResponse()
+    {
+        //TODO error handling
+        $lastMonth = Carbon::now()->subMonth()->format('Y-m-d');
+        $url ='https://api.github.com/search/repositories?q=created:>'.$lastMonth.'&sort=stars&order=desc';
+        $this->response =  Http::get($url)->json();
+
+    }
+
+
     public function trendingLanguages()
     {
-        return null;
+        return gettype($this->response['items']);
     }
 }
